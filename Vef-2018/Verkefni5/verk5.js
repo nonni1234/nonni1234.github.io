1f8b72c56d960039c5c00e6861256fbb6fdd8c11
Jon Bjarki <nonni1234567@gmail.com>
2019-01-11 09:11:44 +0000

    Vef-2018

---
+++ Vef-2018/Verkefni5/verk5.js
@@ -0,0 +1,88 @@
+function myFunction() {
+    var x = document.getElementById("lysing");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+
+
+function myFunction2() {
+    var x = document.getElementById("ahugamal");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+
+
+function myFunction3() {
+    var x = document.getElementById("verkefni");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+
+function myFunction4() {
+    var x = document.getElementById("buttons");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function myFunction5() {
+    var x = document.getElementById("sjalfsmynd");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function Mynd1() {
+    var x = document.getElementById("mynd1");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function Mynd2() {
+    var x = document.getElementById("mynd2");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function Mynd3() {
+    var x = document.getElementById("mynd3");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function Mynd4() {
+    var x = document.getElementById("mynd4");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function Grein() {
+    var x = document.getElementById("grein");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
